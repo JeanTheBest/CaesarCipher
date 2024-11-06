0f80218bc86382bc7176f25f282d237cfe30c8dc
JeanTheBest <151757136+JeanTheBest@users.noreply.github.com>
2024-11-06 22:06:41 +0100

    codeur.py
    j'ai fini ma partie

--- caesar2.py
+++ caesar2.py
@@ -1,25 +1,43 @@
+
+######################################################
+############# BIBLIOTHEQUE - FONCTIONS ###############
+######################################################
+
 def codage(caractere, decalage):
-    caractere = ord(caractere) 
-    if 65 <= caractere <= 90:
+    caractere = ord(caractere)
+
+    if 65 <= caractere <= 90:  # Majuscules (A-Z)
         caractere_code = caractere + decalage
         if caractere_code > 90:
+            caractere_code -= 26 # Si caractère dépasse z ( code asci 122), on fait revenir la lettre au début de l'alphabet
+        return chr(caractere_code)
+
+    elif 97 <= caractere <= 122:  # Minuscules (a-z)
+        caractere_code = caractere + decalage
+        if caractere_code > 122:
             caractere_code -= 26
         return chr(caractere_code)
+
     else:
-        return caractere
+        return chr(caractere)  # Autres caractères inchangés
 
-question = input("Voulez-vous coder un message ? (o/n) : ")
 
+
+###########################################
+############# CODE PRINCIPAL ##############
+###########################################
+
+question = input("Voulez-vous coder un message ? o/n : ")
 if question == "o":
-    message = input("Rentrez le message à coder : ")
+    message = input("Entrez le message : ")
     decalage = 3
     message_code = ""
 
+    # Parcours chaque lettre du message pour la coder
     for lettre in message:
-        message_code += codage(lettre, decalage) 
+        message_code += codage(lettre, decalage)
 
     print("Message codé : ", message_code)
+    
 elif question == "n":
     print("A bientôt")
-else:
-    print("Veuillez entrer 'o' ou 'n'")
