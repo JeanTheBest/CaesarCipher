4c37491bad637f7b033cb808b6ccdc0eac724760
JeanTheBest <151757136+JeanTheBest@users.noreply.github.com>
2024-11-13 18:54:30 +0100

    codeur.py
    correction de détails mineurs

--- codeur.py
+++ codeur.py
@@ -9,13 +9,13 @@ def codage(caractere, decalage):
     if 65 <= caractere <= 90:  # Majuscules (A-Z)
         caractere_code = caractere + decalage
         if caractere_code > 90:
-            caractere_code -= 26 # Si caractère dépasse z ( code asci 122), on fait revenir la lettre au début de l'alphabet
+            caractere_code = caractere_code - 26 # si caractère dépasse z , on fait revenir la lettre au début de l'alphabet
         return chr(caractere_code)
 
-    elif 97 <= caractere <= 122:  # Minuscules (a-z)
+    elif 97 <= caractere <= 122:  # minuscules (a-z)
         caractere_code = caractere + decalage
         if caractere_code > 122:
-            caractere_code -= 26
+            caractere_code = caractere_code - 26
         return chr(caractere_code)
 
     else:
@@ -35,9 +35,9 @@ if question == "o":
 
     # Parcours chaque lettre du message pour la coder
     for lettre in message:
-        message_code += codage(lettre, decalage)
+        message_code = message_code + codage(lettre, decalage)
 
     print("Message codé : ", message_code)
-    
+
 elif question == "n":
     print("A bientôt")
